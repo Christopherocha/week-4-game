e41cc7bedf63f4eea475a8e2d491be1118da111c
Chris R <chris.rocha@>
2017-05-06 09:49:11 -0500

    adding latest changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,224 @@
 var characterSet = false;
-var chosenCharacter = ;
-var currentEnemy= ;
+var chosenCharacter = false;
+var currentEnemy= false;
+var enemiesLeft = 3;
+var winCount = 0;
 
 var players = {
 	player1:{
 		name: "Erlich",
-		healthPoints: ,
-		attack: ,
-		counterAttack:
+		healthPoints: 120,
+		attack: 5,
+		baseAttack: 5,
+		counterAttack: 20,
+		img: "assets/images/erlich.jpeg"
 	},
 	player2:{
 		name: "Dinesh",
-		healthPoints: ,
-		attack: ,
-		counterAttack:
+		healthPoints: 100,
+		attack: 7,
+		baseAttack: 7,
+		counterAttack: 15,
+		img: "assets/images/dinesh.jpeg"
 	},
 	player3:{
 		name: "Gilfoyle",
-		healthPoints: ,
-		attack: ,
-		counterAttack:
+		healthPoints: 150,
+		attack: 10,
+		baseAttack: 10,
+		counterAttack: 20,
+		img: "assets/images/gilfoyle.jpeg"
 	},
 	player4:{
 		name: "Gavin",
-		healthPoints: ,
-		attack: ,
-		counterAttack: 25;
+		healthPoints: 180,
+		attack: 10,
+		baseAttack: 10,
+		counterAttack: 25,
+		img: "assets/images/gavin.jpeg"
 	}
 }
 
-$(document).ready( function() {
+$(document).ready(function() {
 
+	// Choose character logic
 	$("#choose-erlich").on("click", function() {
 		characterSet = true;
 		chosenCharacter = players.player1;
 
 		$("#characters").hide();
-
+		$("#fight-erlich").hide();
+		$("#chosen-erlich").show();
+		$("#chosen").show();
+		$("#enemy-row").show();
+		$("#chosen-name").text(chosenCharacter.name);
+		$("#chosen-img").attr("src", chosenCharacter.img);
+		$("#chosen-hp").text(chosenCharacter.healthPoints);
 	});
 
 	$("#choose-dinesh").on("click", function() {
-			characterSet = true;
-			chosenCharacter = players.player2;
+		characterSet = true;
+		chosenCharacter = players.player2;
+
+		$("#characters").hide();
+		$("#fight-dinesh").hide();
+		$("#chosen-dinesh").show();
+		$("#chosen").show();
+		$("#enemy-row").show();
+		$("#chosen-name").text(chosenCharacter.name);
+		$("#chosen-img").attr("src", chosenCharacter.img);
+		$("#chosen-hp").text(chosenCharacter.healthPoints);
 	});
 
 	$("#choose-gilfoyle").on("click", function() {
-			characterSet = true;
-			chosenCharacter = players.player3;
+		characterSet = true;
+		chosenCharacter = players.player3;
+
+		$("#characters").hide();
+		$("#fight-gilfoyle").hide();
+		$("#chosen-gilfoyle").show();
+		$("#chosen").show();
+		$("#enemy-row").show();
+		$("#chosen-name").text(chosenCharacter.name);
+		$("#chosen-img").attr("src", chosenCharacter.img);
+		$("#chosen-hp").text(chosenCharacter.healthPoints);
 	});
 
 	$("#choose-gavin").on("click", function() {
-			characterSet = true;
-			chosenCharacter = players.player4;
+		characterSet = true;
+		chosenCharacter = players.player4;
+
+		$("#characters").hide();
+		$("#fight-gavin").hide();
+		$("#chosen-gavin").show();
+		$("#chosen").show();
+		$("#enemy-row").show();
+		$("#chosen-name").text(chosenCharacter.name);
+		$("#chosen-img").attr("src", chosenCharacter.img);
+		$("#chosen-hp").text(chosenCharacter.healthPoints);
+	});
+	// End choose character logic
+
+	// Choose enemy logic
+
+	$("#fight-erlich").on("click", function() {
+		if (currentEnemy === false) {
+			currentEnemy = players.player1;
+			$("#fight-erlich").hide();
+			$("#enemy-row").hide();
+			$("#defender-name").text(currentEnemy.name);
+			$("#defender-img").attr("src", currentEnemy.img);
+			$("#defender-hp").text(currentEnemy.healthPoints);
+			
+			if (enemiesLeft < 3) {
+				$("#opponent").show();
+			}
+		} else {
+			// logic to mention that opponent already chosen
+		}
 	});
 
+	$("#fight-dinesh").on("click", function() {
+		if (currentEnemy === false) {
+			currentEnemy = players.player2;
+			$("#fight-dinesh").hide();
+			$("#enemy-row").hide();
+			$("#defender-name").text(currentEnemy.name);
+			$("#defender-img").attr("src", currentEnemy.img);
+			$("#defender-hp").text(currentEnemy.healthPoints);
+			
+			if (enemiesLeft < 3) {
+				$("#opponent").show();
+			}
+		} else {
+			//logic to mention that opponent already chosen
+		}
+	});
 
+	$("#fight-gilfoyle").on("click", function() {
+		if (currentEnemy === false) {
+			currentEnemy = players.player3;
+			$("#fight-gilfoyle").hide();
+			$("#enemy-row").hide();
+			$("#defender-name").text(currentEnemy.name);
+			$("#defender-img").attr("src", currentEnemy.img);
+			$("#defender-hp").text(currentEnemy.healthPoints);
+			
+			if (enemiesLeft < 3) {
+				$("#opponent").show();
+			}
+		} else {
+			//logic to mention that opponent already chosen
+		}
+	});
+
+	$("#fight-gavin").on("click", function() {
+		if (currentEnemy === false) {
+			currentEnemy = players.player4;
+			$("#fight-gavin").hide();
+			$("#enemy-row").hide();
+			$("#defender-name").text(currentEnemy.name);
+			$("#defender-img").attr("src", currentEnemy.img);
+			$("#defender-hp").text(currentEnemy.healthPoints);
+
+			if (enemiesLeft < 3) {
+				$("#opponent").show();
+			}
+		} else {
+			//logic to mention that opponent already chosen
+		}
+	});
+
+	// End logic to choose opponent
+
+	// Begin attack logic
+	$("#attack").on("click", function(){
+		if (currentEnemy === false) {
+			// do something to alert the user to pick an enemy
+
+
+		} else {
+			// logic to attack the opponent and then also to check if their health is below 0 to remove them from the currentOpponent ...
+			// ... section and also check the count for defeated enemies to display the restart button.
+			currentEnemy.healthPoints = currentEnemy.healthPoints - chosenCharacter.attack;
+			chosenCharacter.healthPoints = chosenCharacter.healthPoints - currentEnemy.counterAttack;
+			chosenCharacter.attack += chosenCharacter.baseAttack; 
+			$("#defender-hp").text(currentEnemy.healthPoints);
+			$("#chosen-hp").text(chosenCharacter.healthPoints);
+
+			if (currentEnemy.healthPoints <= 0) {
+				currentEnemy = false;
+				enemiesLeft --;
+				$("#enemy-row").show();
+				$("#opponent").hide();
+				$("#defender-name").text("");
+				$("#defender-img").attr("src", "");
+				$("#defender-hp").text("");
+				// add logic to display message to choose a new opponent
+				if (enemiesLeft === 0) {
+					// Logic to restart the game
+					characterSet = false;
+					chosenCharacter = false;
+					currentEnemy= false;
+					enemiesLeft = 3;
+
+					$("#characters").show();
+					$("#enemy-row").hide();
+					$("#fight-erlich").show();
+					$("#fight-dinesh").show();
+					$("#fight-gilfoyle").show();
+					$("#fight-gavin").show();
+
+					//Hide enemy row and make sure all
+
+				}
+
+			} else if (chosenCharacter.healthPoints <= 0) {
+				//logic for gameover bc your person died
+
+			}
+		}
+
+	});
 
 });
